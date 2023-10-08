e130eb17682f0ad879eb9f5d0a28f0a2d052fa60
miguelgargallo <it@itamaesan.org>
2023-10-08 21:32:52 +0200

    001

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,12 @@ export default function Home() {
       <h1 className="text-5xl font-bold col-span-4">Love From Miguel</h1>
       <h2 className="text-3xl font-semibold">A Collection Close to My Heart</h2>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {/* ... existing code ... */}
-
         <div className="video-container">
           <iframe
             width="560"
             height="315"
             src="https://www.youtube.com/embed/_TedFmvfCYo"
             title="YouTube video player"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
