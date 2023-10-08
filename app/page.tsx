1e00d2f57dadead2715a730a4ab432a22150274e
miguelgargallo <it@itamaesan.org>
2023-10-08 21:50:08 +0200

    001

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold col-span-4">Love From Miguel</h1>
       <h2 className="text-3xl font-semibold">A Collection Close to My Heart</h2>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="video-container items-center justify-center">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/_TedFmvfCYo"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-        <div className="col-span-4 mt-8">
-          <p className="text-lg">
-            I created this collection to share my love for Vero & Sam. I hope you enjoy
-            it as much as I do!
-          </p>
-        </div>
+      <iframe
+        width="720"
+        height="480"
+        src="https://www.youtube.com/embed/_TedFmvfCYo"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <div className="col-span-4 mt-8">
+        <p className="text-lg">
+          I created this collection to share my love for Vero & Sam. I hope you enjoy
+          it as much as I do!
+        </p>
       </div>
     </main>
   )
